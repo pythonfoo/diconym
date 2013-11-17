94e718382a60d948642f1e898103a8f01ede2c7a
dodonator <dodo@dodo@chaosdorf.de>
2013-11-17 17:06:41 +0100

    laden

---
+++ LadenUndSpeichernWL/ladenWL.py
@@ -0,0 +1,38 @@
+# Laden
+def read_file(filename):
+	file1 = open(filename,'r')
+	result = file1.read()
+	file1.close()
+	return result
+
+def read_line(filecontent):
+	result = filecontent.split('\n')
+	return result
+
+def ignore_lines_sharp(lines):
+	result = []
+	for line in lines:
+		if list(line)[0] == '#':
+			pass
+		else:
+			result.append(line)
+
+def ignore_lines_spaces(lines):
+	result = []
+	tmpLine = ''
+	for line in lines:
+		for i in range(len(line)):
+			if list(line)[i] != ' ':
+				tmpLine += list(line)[i]
+			else:
+				break
+		result.append(tmpLine)
+		tmpLine = ''
+	return result
+
+def main(filename):
+	step1 = read_file(filename)
+	step2 = read_line(step1)
+	step3 = ignore_lines_sharp(step2)
+	step4 = ignore_lines_spaces(step3)
+	return step4
