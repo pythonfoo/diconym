330b7b6f4714f9aafa43ee3b05fbc2a44fd5389c
dodonator <dodo@dodo@chaosdorf.de>
2013-11-17 19:13:14 +0100

    laden update dodo

--- LadenUndSpeichernWL/ladenWL.py
+++ LadenUndSpeichernWL/ladenWL.py
@@ -1,55 +1,54 @@
 # Laden
-def read_file(filename):
-	file1 = open(filename,'r')
-	result = file1.read()
-	file1.close()
-	return result
+class whitelist(object):
+	def __init__(self,filename):
+		self.filename = filename
+		step1 = read_file(filename)
+		step2 = read_line(step1)
+		step3 = ignore_lines_sharp(step2)
+		step4 = ignore_lines_spaces(step3)
+		step5 = string_converter(step4)
+		return step5
 
-def read_line(filecontent):
-	result = filecontent.split('\n')
-	return result
+	def read_file(self,filename):
+		file1 = open(filename,'r')
+		result = file1.read()
+		file1.close()
+		return result
 
-def ignore_lines_sharp(lines):
-	result = []
-	for line in lines:
-		if list(line)[0] == '#':
-			pass
-		else:
-			result.append(line)
-	return result
+	def read_line(self,filecontent):
+		result = filecontent.split('\n')
+		return result
 
-def ignore_lines_spaces(lines):
-	result = []
-	tmpLine = ''
-	for line in lines:
-		for i in range(len(line)):
-			if list(line)[i] != '	' and list(line)[i] != ' ':
-				tmpLine += list(line)[i]
+	def ignore_lines_sharp(self,lines):
+		result = []
+		for line in lines:
+			if list(line)[0] == '#':
+				pass
 			else:
-				break
-		result.append(tmpLine)
+				result.append(line)
+		return result
+
+	def ignore_lines_spaces(self,lines):
+		result = []
 		tmpLine = ''
-	return result
+		for line in lines:
+			for i in range(len(line)):
+				if list(line)[i] != '	' and list(line)[i] != ' ':
+					tmpLine += list(line)[i]
+				else:
+					break
+			result.append(tmpLine)
+			tmpLine = ''
+		return result
 
-def string_converter(lines):
-	result = []
-	tmpPart1 = ''
-	tmpPart2 = ''
-	for line in lines:
-		tmp = line.split(',')
-		tmpPart1 = tmp[0]
-		tmpPart2 = tmp[1]
-		result.append((tmpPart1,tmpPart2))
-	
-	return result
-
-
-def main(filename):
-	step1 = read_file(filename)
-	step2 = read_line(step1)
-	step3 = ignore_lines_sharp(step2)
-	step4 = ignore_lines_spaces(step3)
-	step5 = string_converter(step4)
-	return step5
-	
-print main('keys2hold.txt')
+	def string_converter(self,lines):
+		result = []
+		tmpPart1 = ''
+		tmpPart2 = ''
+		for line in lines:
+			tmp = line.split(',')
+			tmpPart1 = tmp[0]
+			tmpPart2 = tmp[1]
+			result.append((tmpPart1,tmpPart2))
+		
+		return result
