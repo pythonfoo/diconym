ddd4f849a5f39f75f8ce56d272be2b2314bb25de
bison-- <mike.bison42@googlemail.com>
2013-12-08 21:41:08 +0100

    anon test: read whitelist

--- tests/anon.py
+++ tests/anon.py
@@ -8,8 +8,14 @@ path, filename = os.path.split(fPath)
 sys.path.append(path)
 
 #from .. import core
-from ..LadenUndSpeichernWL  import ladenWL2 as lW
+from LadenUndSpeichernWL  import ladenWL2 as lW
 
+
+print "anon test"
+listRootPath = os.path.abspath('../lists')
 wl = lW.WhiteList()
-print wl.mainListDir("lists/tags_to_anonymize")
+#print wl.mainListDir(os.path.abspath('../lists') + "/tags_to_anonymize")
+categorys = wl.categoryListFiles(listRootPath)
 
+categoryNeverDel = categorys['tags_to_hold']
+print wl.mainReadWhiteList(listRootPath + '/tags_to_hold/' +  categoryNeverDel[0] + ".txt")
