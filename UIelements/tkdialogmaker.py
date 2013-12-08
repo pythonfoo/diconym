edaff0dbc24451a71ee2ffaf6158bb5d5a3cc1df
Michael Stehmann <michael@lando>
2013-12-08 20:48:04 +0100

    Und weiter, immer weiter

--- UIelements/tkdialogmaker.py
+++ UIelements/tkdialogmaker.py
@@ -23,6 +23,8 @@
 from Tkinter import *
 import tkMessageBox
 from tkFileDialog import *
+from tkFont import *
+import os
 from gettextintegration import TranslationIntegration
 
 
@@ -78,6 +80,17 @@ class DialogMaker(object):
 		self.listbox.grid(row=r, column=c, padx=5)
 		return self.listbox
 
+	def tablelistbox(self, liste, w, r, c):
+		"""Erzeugt ein Listenfeld"""
+		self.listbox = Listbox(self.dia, width=w)
+		_fontl = Font(family='Courier', size=10, underline=1)
+		self.listbox.config(font=_fontl)
+		#self.listbox.config(font=('Courier 10 Pitch', 10))
+		for i in range(0,len(liste)):
+			self.listbox.insert("end", "  "+liste[i])
+		self.listbox.grid(row=r, column=c, padx=5)
+		return self.listbox
+
 	def auswahl(self, listbox):
 		"""Stellt die im Listenfeld des Dialoges gewaehlte Auswahl fest"""
 		auswahl=listbox.get("active").strip()
@@ -97,15 +110,15 @@ class DialogMaker(object):
 		return e
 
 	def minfo(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showinfo(mbtitle, mbtext, type=btype)
+		sb = tkMessageBox.showinfo(mbtitle, mbtext, type=btype)
 		return sb
 
 	def mwarning(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showwarning(mbtitle, mbtext, type=btype)
+		sb = tkMessageBox.showwarning(mbtitle, mbtext, type=btype)
 		return sb
 
 	def merror(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showerror(mbtitle, mbtext, type=btype)
+		sb = tkMessageBox.showerror(mbtitle, mbtext, type=btype)
 		return sb
 
 	def fileOpenDialog(self):
