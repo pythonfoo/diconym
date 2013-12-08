f67b901b05013ad53681d503a378eba0e244bb26
Michael Stehmann <michael@lando>
2013-12-08 13:45:32 +0100

    Grafik modularisiert

---
+++ UIelements/gettextintegration.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#       gettextintegration.py
+#       
+#       Copyright 2013 Michael Stehmann <info@rechtsanwalt-stehmann.de>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+# Begin gettext integration
+
+class TranslationIntegration(object):
+	"""GNU-gettext for GNU/Linux"""
+
+	def __init__(self, pname):
+
+		self.moname = pname + ".mo"
+		self.syslanguage()
+		# check, whether it's a unix system installation
+		_localepath = "/usr/share/locale/"
+
+		_result = self.look4mo(_localepath)
+
+		if _result == "xxx":
+			self.syslanguage()
+			_localepath = "locale/"
+			_result = self.look4mo(_localepath)
+
+		if not _result == "xxx":
+			# chooses the right .mo file and installs translation
+			trans = gettext.translation(pname, _localepath, [self.language])
+			trans.install()
+		else:
+			# Translating the following strings is absurd!
+			serror = "Bad package! No "+self.moname+" file found!"
+			from tkMessageBox import *
+			messagebox.showerror('Error!', serror)
+			import sys; sys.exit(1)
+
+	def syslanguage(self):
+		"""which is the language of the system (GNU/Linux)?"""
+		_loclang = locale.getdefaultlocale()
+		self.language = _loclang[0]
+		return self.language
+
+	def look4mo(self, localepath):
+		"""choose only a language which a .mo file exists
+		for, else use english or return 'xxx' """
+		_lpath = localepath + self.language + "/LC_MESSAGES/" + self.moname
+		
+		if not os.path.exists(_lpath):
+
+			_newlang = self.dosplit(self.language, "_")
+			_lpath = localepath + _newlang + "/LC_MESSAGES/"+self.moname
+
+			if os.path.exists(_lpath):
+				self.language = _newlang
+			else:
+				_newlang = self.dosplit(self.language, "@")
+				_lpath = localepath + _newlang + "/LC_MESSAGES/" + self.moname
+
+				if os.path.exists(_lpath):
+					self.language = _newlang
+				else:     
+					_lpath = localepath + "en/LC_MESSAGES/" + self.moname
+
+					if os.path.exists(_lpath):
+						self.language = "en"
+					else:
+						self.language = "xxx"
+
+		return self.language
+
+	def dosplit(self, language, splitter):
+
+		"""
+			splits language descriptor
+
+			>>> gnugettext.dosplit(gnugettext, "de_DE", "_")
+			'de'
+
+			>>> gnugettext.dosplit(gnugettext, "be@latin", "@")
+			'be'
+		"""
+
+		_newlang = language.split(splitter)
+		_newlang = _newlang[0]
+		
+		return _newlang
+		
+# End gettext integration
--- diconymgraphics.py
+++ diconymgraphics.py
@@ -23,122 +23,9 @@
 # In Debian muessen folgende Pakete zusaetzlich installiert werden:
 # -- python-tk
 
-from Tkinter import *
-from tkMessageBox import *
-from tkFileDialog import *
+from UIelements.tkdialogmaker import DialogMaker
+from UIelements.gettextintegration import TranslationIntegration
 import core
-
-class DialogMaker(object):
-	"""Methoden zur Erzeugung und Gestaltung von Tkinter-Dialogen"""
-
-	def __init__(self):
-		"""Erzeugt Tkinter-Dialog und setzt Dialogtitel"""
-		self.dia=Tk()
-
-	def title (self, title):
-		self.diatitle(title)
-
-	def diatitle (self, title):
-		"""Setzt den Titel des Dialogs"""
-		self.dia.title(title)
-
-	def mainloop(self):
-		"""Erzeugt Dialog"""
-		self.dia.mainloop()
-
-	def button(self, btext, befehl, r, c):
-		"""Erzeugt den Auswahlbutton"""
-		auswahlbutton = Button(self.dia, text = btext, command = befehl)
-		auswahlbutton.grid(row=r, column=c, padx=10)
-
-	def endebutton(self, r, c):
-		"""Erzeugt den Button zum Beenden"""
-		eb = Button(self.dia, text = "Ende", fg="red", command = self.ende)
-		eb.grid(row=r, column=c)
-
-	def ende(self):
-		"""Beendet den Dialog"""
-		self.dia.destroy()
-
-	def label(self, ausgabetext, w, r, c, bgc, cs=1):
-		"""Erzeugt ein Anzeigefeld"""
-		lb = Label(self.dia, text = ausgabetext, width=w, bg=bgc)
-		lb.grid(row=r, column=c, columnspan=cs)
-
-	def eingabeFeld(self, w, r, c, cs=1):
-		"""Erzeugt ein Eingabefeld"""
-		e = Entry(self.dia, width=w)
-		e.grid(row=r, column=c, columnspan=cs)
-		return e
-
-	def listbox(self, liste, w, r, c):
-		"""Erzeugt ein Listenfeld"""
-		self.listbox = Listbox(self.dia, width=w)
-		for i in range(0,len(liste)):
-			self.listbox.insert("end", "  "+liste[i])
-		self.listbox.grid(row=r, column=c, padx=5)
-		return self.listbox
-
-	def auswahl(self, listbox):
-		"""Stellt die im Listenfeld des Dialoges gewaehlte Auswahl fest"""
-		auswahl=listbox.get("active").strip()
-		return auswahl
-
-	# Allgemeine Elemente
-
-	def label(self, ausgabetext, w, r, c, bgc, cs=1):
-		"""Erzeugt ein Anzeigefeld"""
-		lb = Label(self.dia, text = ausgabetext, width=w, bg=bgc)
-		lb.grid(row=r, column=c, columnspan=cs)
-
-	def eingabeFeld(self, w, r, c, cs=1):
-		"""Erzeugt ein Eingabefeld"""
-		e = Entry(self.dia, width=w)
-		e.grid(row=r, column=c, columnspan=cs, padx=20)
-		return e
-
-	def minfo(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showinfo(mbtitle, mbtext, type=btype)
-		return sb
-
-	def mwarning(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showwarning(mbtitle, mbtext, type=btype)
-		return sb
-
-	def merror(self, mbtitle, mbtext, btype="ok"):
-		sb = messagebox.showerror(mbtitle, mbtext, type=btype)
-		return sb
-
-	def fileOpenDialog(self):
-		filename = askopenfilename(filetypes = [('all files','*.*')],title ="Suche nach Dicom-Verzeichnissen")
-		return(filename)
-		
-	def folderOpenDialog(self):
-		foldername = askdirectory(title ="Suche nach Dicom-Verzeichnissen")
-		return(foldername)
-
-	def statusbar(self, sbtext, r, c, cs=1):
-		pad = 1
-		self.status = Label(self.dia, text="", bd=1, relief=SUNKEN, anchor=W)
-		self.status.config(text=sbtext)
-		self.status.grid(row=r, columnspan=cs, column=c, sticky=E+W, pady=pad, padx=pad)
-
-	def menu(self):
-		self.menu = Menu(self.dia)
-		self.dia.config(menu=self.menu)
-
-	def topmenu(self, topname):
-		self.topmenu = Menu(self.menu)
-		self.menu.add_cascade(label=topname, menu=self.topmenu)
-
-	def menuentry(self, mlabel, mcommand):
-		self.topmenu.add_command(label=mlabel, command=mcommand)
-
-	def menuseparator(self):
-		self.topmenu.add_separator()
-
-	def exitmenupoint(self):
-		self.topmenu.add_command(label="Exit", command=self.ende)
 
 
 class MainDialog(object):
@@ -187,6 +74,7 @@ or (at your option) any later version."""
 
 def main():
 
+	ti = TranslationIntegration(diconym)
 	start = MainDialog()
 	start.startdia()
 
